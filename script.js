dbecc0ff5b87d78689876e2ac54615006f39d49a
nawafchehade <CHEHADENAWAF@GMAIL.COM>
2024-11-11 23:51:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Optional JavaScript for smooth scroll effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth
